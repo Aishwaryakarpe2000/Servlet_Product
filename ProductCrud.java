662d9b43014c1a5747e2b23a726e4b03488442f6
Aishwaryakarpe2000 <137754728+Aishwaryakarpe2000@users.noreply.github.com>
2023-10-22 14:23:52 +0530

    Add files via upload

---
+++ ProductCrud.java
@@ -0,0 +1,27 @@
+package servlets_product1;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+
+public class ProductCrud {
+
+	
+	public int saveProduct(Product p) throws Exception 
+	{
+		//1.
+		Class.forName("com.mysql.cj.jdbc.Driver");
+		//2.establish connection
+		Connection con=DriverManager.getConnection("jdbc:mysql://localhost:3306/prodservdb?user=root&password=root");
+		//3.
+		PreparedStatement ps=con.prepareStatement("insert into product values(?,?,?,?,?)");
+		ps.setInt(1, p.getId());
+		ps.setString(2, p.getName());
+		ps.setDouble(3, p.getPrice());
+		ps.setString(4, p.getBrand());
+		ps.setString(5, p.getState());
+		
+		int count=ps.executeUpdate();
+		return count;
+	}
+}
