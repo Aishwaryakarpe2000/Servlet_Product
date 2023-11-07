3a3bd087cd690817806ae4fd994c15ef0e68e422
Aishwaryakarpe2000 <137754728+Aishwaryakarpe2000@users.noreply.github.com>
2023-11-07 11:53:04 +0530

    Update ProductCrud.java

--- ProductCrud.java
+++ ProductCrud.java
@@ -9,11 +9,10 @@ public class ProductCrud {
 	
 	public int saveProduct(Product p) throws Exception 
 	{
-		//1.
 		Class.forName("com.mysql.cj.jdbc.Driver");
-		//2.
+
 		Connection con=DriverManager.getConnection("jdbc:mysql://localhost:3306/prodservdb?user=root&password=root");
-		//3.
+	
 		PreparedStatement ps=con.prepareStatement("insert into product values(?,?,?,?,?)");
 		ps.setInt(1, p.getId());
 		ps.setString(2, p.getName());
